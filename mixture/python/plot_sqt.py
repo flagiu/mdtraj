fixed S(q,t) computation and improved plotting

mixture/python/plot_sqt.py
@@ -13,8 +13,8 @@ plt.rcParams['figure.dpi'] = 200
 
 parser = argparse.ArgumentParser(
                     prog = sys.argv[0],
-                    description = 'Plots the Angular-Limited Three-Body Correlation as a 2D heatmap.',
-                    epilog='End of the summary.'
+                    description = 'Plots the Intermediate Scattering Function S(q,t).',
+                    epilog='Both as a function of t at parametric q, and vice-versa.'
 )
 parser.add_argument('--inavg',  type=argparse.FileType('r'),
                      default="sqt.ave", required=False,
@@ -23,20 +23,28 @@ parser.add_argument('--inavg',  type=argparse.FileType('r'),
 parser.add_argument('--dt',  type=float, default=0.002, required=False,
                      help="Integration time step (picoseconds). [default: %(default)s]"
 )
-
+parser.add_argument('--select_q',  type=int, nargs="+", default=[], required=False,
+                     help="Plot vs. t only the q's indexed by the selected integers. [default: %(default)s]"
+)
+parser.add_argument('--select_t',  type=int, nargs="+", default=[], required=False,
+                     help="Plot vs. q only the t's indexed by the selected integers. [default: %(default)s]"
+)
 parser.add_argument('--yshift',  type=float, default=0.0, required=False,
                      help="Shift vertically the vs-time-plot. [default: %(default)s]"
 )
 parser.add_argument('--normalize',  type=int, default=1, required=False,
                      help="Normalize the vs-time-plot to S(q,0)? 1: yes ; 0: no. [default: %(default)s]"
 )
-
+parser.add_argument('--ylog',  type=int, default=1, required=False,
+                     help="Use log axis for the vs-q-plot? 1: yes ; 0: no. [default: %(default)s]"
+)
 
 outname="sqt"
 
 #-------------------------------------#
 args = parser.parse_args()
 assert args.normalize==0 or args.normalize==1
+assert args.ylog==0 or args.ylog==1
 
 print("Plotting monospecies S(q,t) ...")
 lines = args.inavg.readlines()
@@ -88,7 +96,36 @@ if Nq==1 or Nt==1:
 else:
     assert sqt.shape==(Nq,Nt)
     assert sqt_.shape==(Nq,Nt)
+print("  Found %d t's and %d q's."%(Nt,Nq))
 
+# Extract S(q,0)=S(q)
+sq = sqt[:,0]
+sq_ = sqt_[:,0]
+
+# Apply selection for t and q
+if args.select_t!=[]:
+    t_selected_idx = args.select_t
+    t_selected = [t[el] for el in t_selected_idx ]
+    print("  Selected t's:",*args.select_t,".")
+    print("          i.e.:",*t_selected,".")
+else:
+    t_selected_idx = np.arange(len(t)) # all indexes
+    t_selected = t
+
+if args.select_q!=[]:
+    q_selected_idx = args.select_q
+    q_selected = [q[el] for el in q_selected_idx ]
+    print("  Selected q's:",*args.select_q,".")
+    print("          i.e.:",*q_selected,".")
+else:
+    q_selected_idx = np.arange(len(q)) # all indexes
+    q_selected = q
+
+def add_upperxticks_to_cbar(cbar,ticks):
+    ax_upper=cbar.ax.twiny()
+    ax_upper.set_xlim(cbar.ax.get_xlim())
+    ax_upper.set_xticks(ticks)
+    ax_upper.set_xticklabels(["%.2f"%el for el in ticks])
 #-------------------------------------------------------------------------------------------------------------------#
 
 fig, axes = plt.subplots(2,2, figsize=(10,5),gridspec_kw={'height_ratios': [1, 9]} ) # figsize is experimental
@@ -98,16 +135,21 @@ ax.set_ylabel(r"$S(q,t)$")
 cmap = mpl.cm.viridis
 norm = mpl.colors.Normalize(vmin=t.min(), vmax=t.max())
 colors = cmap(norm(t))
-for i in range(Nt):
-    lab = r"$t=%.1f$ $ps$"%t[i]
+for ii in range(len(t_selected_idx)):
+    i = t_selected_idx[ii]
+    lab = r"$t=%.1f$ $ps$"%t_selected[ii]
     col = colors[i]
-    y = sqt[:,i] + i*args.yshift
+    y = sqt[:,i]
     y_ = sqt_[:,i]
-    ax.errorbar(q,y,y_, fmt='.-', color=col, label=lab, alpha=0.5)
+    ax.errorbar(q,y,y_, fmt='-', color=col, label=lab, alpha=0.5)
+cbar_t=fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes[0][0],
+                    orientation="horizontal", label="t [ps]")
+if args.select_t!=[]: add_upperxticks_to_cbar(cbar_t,t_selected)
 ax.tick_params(which='both', direction='in')
-ax.set_yscale("log")
-#ax.grid(axis='both', which='major')
-fig.colorbar( mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes[0][0], orientation="horizontal", label="t [ps]")
+if args.ylog:
+    ax.set_yscale("log")
+    ax.set_ylim(max(ax.get_ylim()[0],1e-3),ax.get_ylim()[1])
+
 
 ax = axes[1][1]
 ax.set_xlabel(r"$t$ [$ps$]")
@@ -118,19 +160,24 @@ else:
 cmap = mpl.cm.magma
 norm = mpl.colors.Normalize(vmin=q.min(), vmax=q.max())
 colors = cmap(norm(q))
-for i in range(Nq):
-    lab = r"$q=%.2f$ $\AA^{-1}$"%q[i]
+for ii in range(len(q_selected_idx)):
+    i = q_selected_idx[ii]
+    lab = r"$q=%.2f$ $\AA^{-1}$"%q_selected[ii]
     col = colors[i]
     if args.normalize==1:
-        y = sqt[i] / sqt[i,0] + i*args.yshift
-        y_ = y * np.sqrt( (sqt_[i]/sqt[i])**2 + (sqt_[i,0]/sqt[i,0])**2 ) # gaussian error propagation
+        y = sqt[i] / sq[i]
+        y_ = y * np.sqrt( (sqt_[i]/sqt[i])**2 + (sq_[i]/sq[i])**2 ) # gaussian error propagation
     else:
-        y = sqt[i] + i*args.yshift
+        y = sqt[i]
         y_ = sqt_[i]
-    ax.errorbar(t,y,y_, fmt='.-', color=col, label=lab, alpha=0.5)
+    ax.errorbar(t,y+i*args.yshift,y_, fmt='-', color=col, label=lab, alpha=0.5)
+ax.set_ylim(-0.1,1.1)
+ax.axhline(np.exp(-1), color="k", linestyle="--", linewidth=0.5, zorder=-999)
 ax.tick_params(which='both', direction='in')
 #ax.grid(axis='both', which='major')
-fig.colorbar( mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes[0][1], orientation="horizontal", label=r"q [$\AA^{-1}$]")
+cbar_q=fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=axes[0][1],
+                    orientation="horizontal", label=r"q [$\AA^{-1}$]")
+if args.select_q!=[]: add_upperxticks_to_cbar(cbar_q,q_selected)
 
 plt.tight_layout()
 