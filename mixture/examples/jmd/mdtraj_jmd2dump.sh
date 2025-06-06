d2a8b2ba79a563fcbfe71938a96472573d44ef6a
flagiu <inailuigoivalf@gmail.com>
2025-06-06 15:20:29 +0200

    jmd example

--- mixture/examples/jmd/mdtraj_jmd2dump.sh
+++ mixture/examples/jmd/mdtraj_jmd2dump.sh
@@ -7,15 +7,17 @@ tag=$1     # string tag for output files. Use the empty string ('' or "") for no
 f0=$2      # fraction in [0,1]
 f1=$3      # fraction in [0,1]
 CONFSpath=$4
+typeFile=`dirname $CONFSpath`"/type.dat"
 
 MDTRAJ_PATH="/home/flavio/programmi/mdtraj/mixture"
 
 [[ -e traj.jmd ]] && rm traj.jmd
 ls ${CONFSpath}/pos_* | sort -V | while read el; do cat $el >> traj.jmd; done
-MDTRAJ="${MDTRAJ_PATH}/bin/mdtraj -jmd traj.jmd -fskip $f0 $f1"
+MDTRAJ="${MDTRAJ_PATH}/bin/mdtraj -jmd traj.jmd $typeFile -fskip $f0 $f1"
 [[ $tag ]] && MDTRAJ="${MDTRAJ} -tag $tag" # if tag is not empty, add it as an argument
 [[ $tag ]] && tagdot=".${tag}" || tagdot="" # if tag is not empty, prepend a '.' for output files' names
 
+echo $typeFile
 $MDTRAJ  -out_lammpsdump -pbc_out
 }
 
